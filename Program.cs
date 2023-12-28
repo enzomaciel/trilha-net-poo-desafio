6a750e99dfbe955a078674ba9a170149daa9e15a
enzomaciel <mmaciel.enzo@gmail.com>
2023-12-28 08:31:13 -0300

    Finalizando o desafio de POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone cel1 = new Iphone("21970144016","Iphone10","123456789",64);
+cel1.Numero = "21970149942";
+cel1.InstalarAplicativo("Instagram");
+
+Nokia cel2 = new Nokia("21970144016","Nokia 11","9876454321",64);
+cel2.InstalarAplicativo("Facebook");
